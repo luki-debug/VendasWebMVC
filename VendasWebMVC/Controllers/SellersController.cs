4e3776de5eff8ff60f95b96452193cd0aa0cdbaa
Lucas Gomes <lucas.amocristo@gmail.com>
2021-02-09 21:53:26 -0300

    VendedoresService

---
+++ VendasWebMVC/Controllers/SellersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VendasWebMVC.Services;
+
+namespace VendasWebMVC.Controllers
+{
+    public class SellersController : Controller
+    {
+        private readonly SellerService _sellerServices;
+        public SellersController(SellerService sellerService)
+        {
+            _sellerServices = sellerService;
+        }
+        public IActionResult Index()
+        {
+            var list = _sellerServices.FindAll();
+            return View(list);
+        }
+    }
+}
---
+++ VendasWebMVC/Services/SellerService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VendasWebMVC.Data;
+using VendasWebMVC.Models;
+
+namespace VendasWebMVC.Services
+{
+    public class SellerService
+    {
+        private readonly VendasWebMVCContext _context;
+        public SellerService(VendasWebMVCContext context)
+        {
+            _context = context;
+        }
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
