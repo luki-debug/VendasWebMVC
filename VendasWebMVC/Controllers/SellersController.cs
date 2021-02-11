416c4aac6ac4f24984b25e89d8d880e6d7a92b8d
Lucas Gomes <lucas.amocristo@gmail.com>
2021-02-10 22:30:23 -0300

    Seller details And eager loading

--- VendasWebMVC/Controllers/SellersController.cs
+++ VendasWebMVC/Controllers/SellersController.cs
@@ -59,5 +59,21 @@ namespace VendasWebMVC.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerServices.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
     }
 }
