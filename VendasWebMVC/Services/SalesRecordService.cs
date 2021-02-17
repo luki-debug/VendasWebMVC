4aaeba259936bd18caf334920c10f68438f968a7
Lucas Gomes <lucas.amocristo@gmail.com>
2021-02-17 13:19:20 -0300

    Implementando SimpleSearch

---
+++ VendasWebMVC/Services/SalesRecordService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VendasWebMVC.Data;
+using VendasWebMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace VendasWebMVC.Services
+{
+    public class SalesRecordService
+    {
+        private readonly VendasWebMVCContext _context;
+        public SalesRecordService(VendasWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindBydateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecords select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+    }
+}
